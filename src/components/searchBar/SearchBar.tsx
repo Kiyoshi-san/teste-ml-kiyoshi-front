baa310e87ecba401c971eca511e55dc6fc9f2051
kiyoshi-san <kiyoshibudokan@hotmail.com>
2022-04-28 00:58:33 -0300

    Created api service to bring search word

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -3,14 +3,23 @@ import { Input } from "components/ui/input";
 import { Button } from "components/ui/button";
 import { BsSearch } from "react-icons/bs";
 import { Form } from "components/ui/form";
+import api from "services/api";
 
 const SearchBar: React.FC = () => {
   const [searchVal, setSearchVal] = useState<string>("");
+  const ITEMS_PATH = process.env.NEXT_PUBLIC_ITEMS_PATH || "/api/items";
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
     setSearchVal(e.target.elements["search-input"].value);
-    console.log(searchVal);
+    try {
+      const response = await api.get(`${ITEMS_PATH}?q=${searchVal}`);
+      await console.log(response);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(`Error: ${error.message}`);
+      }
+    }
   };
 
   return (
