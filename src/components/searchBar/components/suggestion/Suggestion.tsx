dc2f41273595b2d7654911d72ac4aee585c7f74c
kiyoshi-san <kiyoshibudokan@hotmail.com>
2022-04-29 21:03:19 -0300

    Suggestion click

--- src/components/searchBar/components/suggestion/Suggestion.tsx
+++ src/components/searchBar/components/suggestion/Suggestion.tsx
@@ -1,8 +1,15 @@
 import { SuggestionModel } from "@models/components/searchBar";
+import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 
 const Suggestion: React.FC = () => {
   const { suggestionList } = useSelector((state: any) => state.searchBar);
+  const router = useRouter();
+
+  const handleProductTileClick = (id: string) => {
+    console.log(id);
+    id && router.push(`/items/${id}`);
+  };
 
   return (
     <div
@@ -15,7 +22,7 @@ const Suggestion: React.FC = () => {
             <div
               key={`suggestion-${index}`}
               className="suggestion-row"
-              onClick={() => console.log(sugL.id)}
+              onClick={() => handleProductTileClick(sugL.id)}
             >
               {sugL.title}
             </div>
